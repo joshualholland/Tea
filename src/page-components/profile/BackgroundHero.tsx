5161521d54ced76c7622d297750684a55250a2dd
Josh <joshualholland@gmail.com>
2021-10-20 00:30:07 -0500

    added s3 domain for images

--- src/page-components/profile/BackgroundHero.tsx
+++ src/page-components/profile/BackgroundHero.tsx
@@ -5,7 +5,12 @@ import Image from 'next/image'
 const BackgroundHero: React.FC = () => (
   <>
     <Hero />
-    <ProfilePhoto />
+    <ProfilePhoto>
+      <Image
+        src='https://cannapi-og.s3.us-east-2.amazonaws.com/vapingvolcano.jpg'
+        layout='fill'
+        alt='User image' />
+    </ProfilePhoto>
   </>
 )
 
@@ -26,6 +31,7 @@ const Hero = styled.div<IProps>`
   background-color: ${props => props.theme.palette.tertiary.main};
   position: relative;
   overflow: hidden;
+  background: linear-gradient(${props => props.theme.palette.tertiary.main}, white);
 
   &:after {
     content: "";
@@ -46,4 +52,5 @@ const ProfilePhoto = styled.div`
   background-color: ${props => props.theme.palette.primary.main};
   border-radius: 50%;
   position: relative;
+  overflow: auto;
 `
